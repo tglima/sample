3ddbce7fb895e56042e68fdc532e9f313fe44ebf
tglima <thg.limadesousa@gmail.com>
2022-08-02 21:07:06 -0300

    initial commit

---
+++ dart/command-line-application/bin/backend.dart
@@ -0,0 +1,7 @@
+//import 'package:shelf/shelf.dart';
+//import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:sample/sample.dart' as app;
+
+void main() {
+  print('Checking application: ${app.checkIsOK()}!');
+}
---
+++ dart/command-line-application/test/sample_test.dart
@@ -0,0 +1,8 @@
+import 'package:backend/sample.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('checkIsOK', () {
+    expect(checkIsOK(), 'OK');
+  });
+}
